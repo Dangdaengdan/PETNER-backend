aa894654ecf579264eadcd1b43a88e284bbf707f
sunm2n <sunmin8410@naver.com>
2025-09-13 10:34:08 +0900

    프로젝트 초기 설정 중

---
+++ src/main/java/com/example/petner/PetnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.petner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petner/PetnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
