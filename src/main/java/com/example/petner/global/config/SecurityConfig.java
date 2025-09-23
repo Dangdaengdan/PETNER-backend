113ee1d8d8f30c0c0d6ee4693fc72ac4d5ab3142
sunm2n <sunmin8410@naver.com>
2025-09-23 09:36:01 +0900

    feature(fonfig):security config 수정(#11)
    - 개발 단계에서 편의를 위해 .anyRequest().permitAll()로 변경
    - 세션 관련 내용 추가

--- src/main/java/com/example/petner/global/config/SecurityConfig.java
+++ src/main/java/com/example/petner/global/config/SecurityConfig.java
@@ -15,10 +15,16 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(authorize -> authorize
                 // Swagger UI 관련 경로 허용
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/ws-stomp/**"  ).permitAll()
-                .anyRequest().authenticated()
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/ws-stomp/**"  ).permitAll()
+                // 개발 단계에서는 모든 요청 허용
+                .anyRequest().permitAll()
             )
-            .csrf(csrf -> csrf.disable());
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session
+                .sessionFixation().migrateSession()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+            );
         
         return http.build();
     }
