e981d19075a06b8407b62e3f09177d71e160d943
sunm2n <sunmin8410@naver.com>
2025-09-23 13:16:20 +0900

    fix(config):(#25)
    - @EnableRedisHttpSession 어노테이션 누락 수정

--- src/main/java/com/example/petner/global/config/SecurityConfig.java
+++ src/main/java/com/example/petner/global/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableWebSecurity
+@EnableRedisHttpSession
 public class SecurityConfig {
 
     @Bean
